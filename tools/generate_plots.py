a7aa6d0a98cabb4f3951c5ffd92a5e66e61f7874
Asad Ahmed <as.ahmed@jacobs-university.de>
2019-12-18 09:52:14 +0100

    Added precision plot

--- tools/generate_plots.py
+++ tools/generate_plots.py
@@ -8,6 +8,7 @@ import pandas as pd
 
 # files paths and constants
 confusion_matrix_file_path = "../fumarole_localization/build/confusion_matrix/test_set_4/classification_confusion_matrix.csv"
+detection_metrics_csv_path = "../fumarole_localization/build/detection_metrics/total_metrics.csv"
 
 def plot_confusion_matrix():
     """ Plot the confusion matrix for the classification evaluation """
@@ -17,6 +18,7 @@ def plot_confusion_matrix():
     class_labels = ["hole", "open_vent", "heated_area", "hidden_vent"]
 
     df_fixed = pd.DataFrame(matrix, class_labels, class_labels).astype(int)
+    print()
     print(df_fixed)
 
     plt.figure(figsize = (10,7))
@@ -26,8 +28,22 @@ def plot_confusion_matrix():
     plt.savefig('output/confusion_matrix.png')
     print("\nHeatmap saved")
 
+
+def plot_detection_precision_plot():
+    """ Plot the location error threshold vs the precision for the detector """
+    df = pd.read_csv(detection_metrics_csv_path)
+
+    plt.figure(figsize = (10,7))
+    ax = sns.lineplot(x="Threshold", y="Precision", data=df)
+    ax.set(xlabel='Center Location Error (Pixels)', ylabel='Precision', title='Detector Precision Plot')
+    plt.savefig('output/detector_precision_plot.png')
+
+    print('\nDetector precision plot saved')
+
+
 def run_main():
-    plot_confusion_matrix()
+    plot_detection_precision_plot()
+    #plot_confusion_matrix()
 
 if __name__ == "__main__":
     run_main()
